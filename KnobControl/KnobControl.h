f9a52ca8035e5ee7a99634df26e737e62e052598
inexcii <ralph.zhouyuan@gmail.com>
2018-08-08 16:07:40 +0900

    2nd commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Yuan Zhou on 2018/08/08.
+//  Copyright © 2018 Yuan Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
